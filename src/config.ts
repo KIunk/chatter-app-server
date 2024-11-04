a75f363a7310b5af229595b59f738c2e7b834120
Klunk <brandonmurry168@gmail.com>
2024-11-03 22:18:47 -0500

    app config

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+import { load } from "jsr:@std/dotenv"
+
+load({
+    envPath: "./d.env",
+    export: true
+})
+
+export const devConfig = {
+    secretKey: Deno.env.get("SECRET_KEY"),
+    port: 3000
+}
